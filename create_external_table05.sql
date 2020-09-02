1d691a61a548455e4aafccb789d0bdba31049364
project303 <32200860+project303@users.noreply.github.com>
2020-09-02 15:29:13 +0700

    Add files via upload

---
+++ create_external_table05.sql
@@ -0,0 +1,18 @@
+USE train05;
+
+DROP TABLE IF EXISTS oozie_data;
+
+CREATE EXTERNAL TABLE IF NOT EXISTS oozie_data (
+    name string,
+    work_place ARRAY<string>,
+    sex_age STRUCT<sex:string,age:int>,
+    skills_score MAP<string,int>,
+    depart_title MAP<STRING,ARRAY<STRING>>
+)
+COMMENT 'This is an external table created by Oozie table'
+ROW FORMAT DELIMITED
+FIELDS TERMINATED BY '|'
+COLLECTION ITEMS TERMINATED BY ','
+MAP KEYS TERMINATED BY ':'
+STORED AS TEXTFILE
+LOCATION '/user/user05/data/oozie_data';
