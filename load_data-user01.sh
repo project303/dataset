dbe1a6ce78330c587b9f197ac501d71f68e2030c
project303 <32200860+project303@users.noreply.github.com>
2020-09-03 12:58:26 +0700

    Update load_data-user01.sh

--- load_data-user01.sh
+++ load_data-user01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mkdir -p /tmp/user01
 wget https://raw.githubusercontent.com/project303/dataset/master/employee.csv -O /tmp/user01/employee.csv
-wget https://raw.githubusercontent.com/project303/dataset/master/load_table05.hql -O /tmp/user01/create_table-user01.hql
+wget https://raw.githubusercontent.com/project303/dataset/master/create_table-user01.hql -O /tmp/user01/create_table-user01.hql
 if [ -f /tmp/user01/employee.csv ]; then
     hdfs dfs -mkdir -p /user/user01/data/oozie_data
     hdfs dfs -put /tmp/user01/employee.csv  /user/user01/data/oozie_data/employee.csv
