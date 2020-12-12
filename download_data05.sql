96792ecc81b93a1c51a8408f1492eb412812fd2c
project303 <32200860+project303@users.noreply.github.com>
2020-12-12 10:44:30 +0700

    Delete download_data05.sql

--- download_data05.sql
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir -p /home/user05/oozie_data
-wget https://raw.githubusercontent.com/project303/dataset/master/employee.csv -P /home/user05/oozie_data
-hdfs dfs -mkdir -p /user/user05/data/oozie_data
-hdfs dfs -put /home/user05/oozie_data/employee.csv  /user/user05/data/oozie_data/employee.csv
